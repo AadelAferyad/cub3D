0496cae6322d6fe83f03b8f47335c5bd1460b70a
aadelAferyad <animevineu@gmail.com>
2025-11-04 18:31:02 +0100

    vec lib

---
+++ includes/vec2d.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   vec2d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aaferyad <aaferyad@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/04 11:44:49 by aaferyad          #+#    #+#             */
+/*   Updated: 2025/11/04 12:11:46 by aaferyad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef VEC2D_H
+# define VEC2D_H
+
+typedef struct s_vector_2d
+{
+	double	x;
+	double	y;
+} t_vec2;
+
+
+t_vec2	vec_add(t_vec2 a, t_vec2 b);
+t_vec2	vec_sub(t_vec2 a, t_vec2 b);
+t_vec2	vec_scale(t_vec2 a, double scale);
+double	vec_dot(t_vec2 a, t_vec2 b);
+double	vec_lenghth(t_vec2 v);
+t_vec2	vec_normalize(t_vec2 v);
+t_vec2	vec_rotate(t_vec2 v, double angle);
+
+#endif
---
+++ vector2d.c
@@ -0,0 +1,88 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   vector2d.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aaferyad <aaferyad@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/04 11:52:21 by aaferyad          #+#    #+#             */
+/*   Updated: 2025/11/04 12:14:47 by aaferyad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <vec2d.h>
+#include <stdio.h>
+
+t_vec2	vec_add(t_vec2 a, t_vec2 b)
+{
+	t_vec2	c;
+
+	c.x = a.x + b.x;
+	c.y = a.y + b.y;
+	return (c);
+}
+
+t_vec2	vec_sub(t_vec2 a, t_vec2 b)
+{
+	t_vec2	c;
+
+	c.x = a.x - b.x;
+	c.y = a.y - b.y;
+	return (c);
+}
+
+t_vec2	vec_scale(t_vec2 a, double scale)
+{
+	t_vec2	c;
+
+	c.x = a.x * scale;
+	c.y = a.y * scale;
+	return (c);
+}
+
+double	vec_dot(t_vec2 a, t_vec2 b)
+{
+	return (a.x * b.x + a.y * b.y);
+}
+
+double	vec_lenghth(t_vec2 v);
+{
+	return (sqrt(v.x * v.x + v.y * v.y));
+}
+
+t_vec2	vec_normalize(t_vec2 v)
+{
+	t_vec2	c;
+	double	len;
+
+	c.x = 0;
+	c.y = 0;
+	len = vec_lenghth(v);
+	if (!len)
+		return (c);
+	c.x = v.x / len;
+	c.y = v.y / len;
+	return (c);
+}
+
+t_vec2	vec_rotate(t_vec2 v, double angle)
+{
+	t_vec2	c;
+
+	c.x = v.x * cos(angle) - v.y * sin(angle);
+	c.y = v.x * sin(angle) + v.y * cos(angle);
+	return (c);
+}
+
+int	main()
+{
+	t_vec2	a;
+	t_vec2	c;
+	t_vec2	b = {1, 2};
+
+	a.x = 1;
+	a.y = 2;
+	c = vec_add(a, b);
+	printf("%f\n%f", c.x, c.y);
+	return (0);
+}
